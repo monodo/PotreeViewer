8dd15bfc81a773385a67e340a8c5bfcad11fed40
monodo <monodo@WSSGRF030.ne.ch>
2015-02-09 10:28:56 +0100

    clean dom & jslint

--- static/js/map.js
+++ static/js/map.js
@@ -2,9 +2,9 @@ pv.initMapView = function () {
 
     // add EPSG:21781 to the proj4 projection database
     proj4.defs('EPSG:21781', "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs ");
-    var swiss = proj4.defs("EPSG:21781");
-    var WGS84 = proj4.defs("WGS84");
-    var webMercator = proj4.defs("EPSG:3857");
+    swiss = proj4.defs("EPSG:21781");
+    WGS84 = proj4.defs("WGS84");
+    webMercator = proj4.defs("EPSG:3857");
 
     // extent of the point cloud (with altitude) in EPSG:21781 / Swiss Coordinate System
     var minSwiss = [589500, 231300, 722.505];
@@ -26,7 +26,7 @@ pv.initMapView = function () {
     var featureVector = new ol.source.Vector({
         features: [feature]
     });
-    
+
     var extentLayer = new ol.layer.Vector({
         source: featureVector,
         style: new ol.style.Style({
@@ -46,11 +46,12 @@ pv.initMapView = function () {
         })
     });
 
-    var visibleBounds = new ol.geom.LineString([
-        minWeb, [maxWeb[0], minWeb[1]], maxWeb, [minWeb[0], maxWeb[1]], minWeb
-    ]);
-
-    var visibleBoundsFeature = new ol.Feature(visibleBounds);
+    if (!visibleBounds) {
+        visibleBounds = new ol.geom.LineString([
+            minWeb, [maxWeb[0], minWeb[1]], maxWeb, [minWeb[0], maxWeb[1]], minWeb
+        ]);
+        var visibleBoundsFeature = new ol.Feature(visibleBounds);
+    }
 
     var visibleBoundsLayer = new ol.layer.Vector({
         source: featureVector,
@@ -71,7 +72,7 @@ pv.initMapView = function () {
         })
     });
 
-    var camFrustumFeatureVector = new ol.source.Vector({
+    camFrustumFeatureVector = new ol.source.Vector({
         features: []
     });
 
@@ -126,11 +127,12 @@ pv.initMapView = function () {
  * update the frustum in the map window according to the camera
  */
 function updateMapFrustum(){
-    if(camFrustum === undefined){
-        var camFrustum = new ol.geom.LineString([ [0,0], [0, 0] ]);
-        var feature = new ol.Feature(camFrustum);
-        camFrustumFeatureVector.addFeature(feature);
-    }
+
+    pv.map.camFrustum = new ol.geom.LineString([ [0,0], [0, 0] ]);
+
+    var feature = new ol.Feature(pv.map.camFrustum);
+    camFrustumFeatureVector.clear();
+    camFrustumFeatureVector.addFeature(feature);
 
     var aspect = camera.aspect;
     var top = Math.tan( THREE.Math.degToRad( camera.fov * 0.5 ) ) * camera.near;
@@ -144,7 +146,6 @@ function updateMapFrustum(){
     camPos.applyMatrix4(camera.matrixWorld);
     left.applyMatrix4(camera.matrixWorld);
     right.applyMatrix4(camera.matrixWorld);
-    
     camPos = pv.toGeo(camPos);
     left = pv.toGeo(left);
     right = pv.toGeo(right);
@@ -153,7 +154,7 @@ function updateMapFrustum(){
     left = proj4(swiss, webMercator, [left.x, left.y]);
     right = proj4(swiss, webMercator, [right.x, right.y]);
 
-    camFrustum.setCoordinates([camPos, left, right, camPos]);
+    pv.map.camFrustum.setCoordinates([camPos, left, right, camPos]);
 }
 
 /**
