2b5ff316d6244b9b6d47de9d799bf116e3a27183
monodo <monodo@WSSGRF030.ne.ch>
2015-02-09 09:11:18 +0100

    DOM cleaning + JSLint

--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,10 @@
 pv.initMapView = function () {
 
-    
     // add EPSG:21781 to the proj4 projection database
     proj4.defs('EPSG:21781', "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs ");
-    swiss = proj4.defs("EPSG:21781");
-    WGS84 = proj4.defs("WGS84");
-    webMercator = proj4.defs("EPSG:3857");
+    var swiss = proj4.defs("EPSG:21781");
+    var WGS84 = proj4.defs("WGS84");
+    var webMercator = proj4.defs("EPSG:3857");
 
     // extent of the point cloud (with altitude) in EPSG:21781 / Swiss Coordinate System
     var minSwiss = [589500, 231300, 722.505];
@@ -15,99 +14,91 @@ pv.initMapView = function () {
     var minWeb = proj4(swiss, webMercator, [minSwiss[0], minSwiss[1]]);
     var maxWeb = proj4(swiss, webMercator, [maxSwiss[0], maxSwiss[1]]);
 
-    { // pointcloud extent layer
-        var extent = [minWeb[0], minWeb[1], maxWeb[0], maxWeb[1]];
-        var center = [(maxWeb[0] + minWeb[0]) / 2, (maxWeb[1] + minWeb[1]) / 2];
-        
-        // draw the extent as box inside the map view
-        var box = new ol.geom.LineString([
-            minWeb, [maxWeb[0], minWeb[1]], maxWeb, [minWeb[0], maxWeb[1]], minWeb
-        ]);
-        
-        
-        var feature = new ol.Feature(box);
-        
-        featureVector = new ol.source.Vector({
-            features: [feature]
-        });
-        
-        var extentLayer = new ol.layer.Vector({
-            source: featureVector,
-            style: new ol.style.Style({
-            fill: new ol.style.Fill({
-              color: 'rgba(255, 255, 255, 0.2)'
-            }),
-            stroke: new ol.style.Stroke({
-              color: '#000000',
-              width: 2
-            }),
-            image: new ol.style.Circle({
-              radius: 3,
-              fill: new ol.style.Fill({
-                color: '#000000'
-              })
-            })
-          })
-        });	
-    }
-    
-    { // visible bounds layer
-        visibleBounds = new ol.geom.LineString([
-            minWeb, [maxWeb[0], minWeb[1]], maxWeb, [minWeb[0], maxWeb[1]], minWeb
-        ]);
-        var visibleBoundsFeature = new ol.Feature(visibleBounds);
-        
-        var visibleBoundsLayer = new ol.layer.Vector({
-            source: featureVector,
-            style: new ol.style.Style({
-            fill: new ol.style.Fill({
-              color: 'rgba(255, 255, 255, 0.2)'
-            }),
-            stroke: new ol.style.Stroke({
-              color: '#0000ff',
-              width: 2
-            }),
-            image: new ol.style.Circle({
-              radius: 3,
-              fill: new ol.style.Fill({
-                color: '#0000ff'
-              })
-            })
-          })
-        });	
-    }
-    
-    
-    { // cam frustum layer
-        camFrustumFeatureVector = new ol.source.Vector({
-            features: []
-        });
+    var extent = [minWeb[0], minWeb[1], maxWeb[0], maxWeb[1]];
+    var center = [(maxWeb[0] + minWeb[0]) / 2, (maxWeb[1] + minWeb[1]) / 2];
 
-        var camFrustumLayer = new ol.layer.Vector({
-            source: camFrustumFeatureVector,
-            style: new ol.style.Style({
+    // draw the extent as box inside the map view
+    var box = new ol.geom.LineString([
+        minWeb, [maxWeb[0], minWeb[1]], maxWeb, [minWeb[0], maxWeb[1]], minWeb
+    ]);
+
+    var feature = new ol.Feature(box);
+    var featureVector = new ol.source.Vector({
+        features: [feature]
+    });
+    
+    var extentLayer = new ol.layer.Vector({
+        source: featureVector,
+        style: new ol.style.Style({
             fill: new ol.style.Fill({
-              color: 'rgba(255, 255, 255, 0.2)'
+                color: 'rgba(255, 255, 255, 0.2)'
             }),
             stroke: new ol.style.Stroke({
-              color: '#000000',
-              width: 2
+                color: '#000000',
+                width: 2
             }),
             image: new ol.style.Circle({
-              radius: 3,
-              fill: new ol.style.Fill({
+                radius: 3,
+                fill: new ol.style.Fill({
                 color: '#000000'
-              })
+                })
             })
-          })
-        });	
-    }
-    
+        })
+    });
+
+    var visibleBounds = new ol.geom.LineString([
+        minWeb, [maxWeb[0], minWeb[1]], maxWeb, [minWeb[0], maxWeb[1]], minWeb
+    ]);
+
+    var visibleBoundsFeature = new ol.Feature(visibleBounds);
+
+    var visibleBoundsLayer = new ol.layer.Vector({
+        source: featureVector,
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                  color: '#0000ff',
+                  width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 3,
+                fill: new ol.style.Fill({
+                    color: '#0000ff'
+                })
+            })
+        })
+    });
+
+    var camFrustumFeatureVector = new ol.source.Vector({
+        features: []
+    });
+
+    var camFrustumLayer = new ol.layer.Vector({
+        source: camFrustumFeatureVector,
+        style: new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 255, 255, 0.2)'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#000000',
+                width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 3,
+                fill: new ol.style.Fill({
+                    color: '#000000'
+                })
+            })
+        })
+    });
+
     // create the map
     pv.map = new ol.Map({
         controls: ol.control.defaults({
             attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-            collapsible: false
+                collapsible: false
             })
         }).extend([
             new ol.control.ZoomToExtent({
@@ -129,15 +120,14 @@ pv.initMapView = function () {
             zoom: 15
         })
     });
+};
     
-}
-    
-    /**
+/**
  * update the frustum in the map window according to the camera
  */
 function updateMapFrustum(){
     if(camFrustum === undefined){
-        camFrustum = new ol.geom.LineString([ [0,0], [0, 0] ]);
+        var camFrustum = new ol.geom.LineString([ [0,0], [0, 0] ]);
         var feature = new ol.Feature(camFrustum);
         camFrustumFeatureVector.addFeature(feature);
     }
@@ -147,7 +137,7 @@ function updateMapFrustum(){
     var bottom = - top;
     var left = aspect * bottom;
     var right = aspect * top;
-    
+
     var camPos = new THREE.Vector3(0, 0, 0);
     left = new THREE.Vector3(left, 0, -camera.near).multiplyScalar(3000);
     right = new THREE.Vector3(right, 0, -camera.near).multiplyScalar(3000);
@@ -158,11 +148,11 @@ function updateMapFrustum(){
     camPos = pv.toGeo(camPos);
     left = pv.toGeo(left);
     right = pv.toGeo(right);
-    
+
     camPos = proj4(swiss, webMercator, [camPos.x, camPos.y]);
     left = proj4(swiss, webMercator, [left.x, left.y]);
     right = proj4(swiss, webMercator, [right.x, right.y]);
-    
+
     camFrustum.setCoordinates([camPos, left, right, camPos]);
 }
 
@@ -172,9 +162,9 @@ function updateMapFrustum(){
 pv.updateMapExtent = function(){
     var geoExtent = pv.toGeo(pointcloud.getVisibleExtent());
     
-    geoMin = proj4(swiss, webMercator, [geoExtent.min.x, geoExtent.min.y]);
-    geoMax = proj4(swiss, webMercator, [geoExtent.max.x, geoExtent.max.y]);
-    
+    var geoMin = proj4(swiss, webMercator, [geoExtent.min.x, geoExtent.min.y]);
+    var geoMax = proj4(swiss, webMercator, [geoExtent.max.x, geoExtent.max.y]);
+
     visibleBounds.setCoordinates([
         geoMin,
         [geoMax[0], geoMin[1]],
@@ -182,4 +172,4 @@ pv.updateMapExtent = function(){
         [geoMin[0], geoMax[1]],
         geoMin
     ]);
-}
+};
