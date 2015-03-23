bee02713b818bd8adda4cff1cfe2b4b838b531a5
monodo <monodo@WSSGRF030.ne.ch>
2015-03-23 11:02:50 +0100

    fix controls bug

--- static/js/potreefunctions.js
+++ static/js/potreefunctions.js
@@ -454,8 +454,8 @@ pv.utils.loop = function () {
 */
 pv.utils.disableControls = function () {
     
-    pv.scene3D.profileTool.setEnabled(false);
-    pv.scene3D.volumeTool.setEnabled(false);
+    pv.scene3D.profileTool.enabled = false;
+    pv.scene3D.volumeTool.enabled = false;
     pv.scene3D.measuringTool.setEnabled(false);
     pv.scene3D.areaTool.setEnabled(false);
     pv.ui.elRenderArea.removeEventListener("click", pv.profile.draw);
