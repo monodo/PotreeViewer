644b607be6997504a1d7cadc22fae5cb75fad446
monodo <olivier.monod@ne.ch>
2015-04-21 10:38:15 +0200

    add new demo cloud

---
+++ cloud/1124322.las/cloud.js
@@ -0,0 +1,29 @@
+{
+	"version": "1.6",
+	"octreeDir": "data",
+	"boundingBox": {
+		"lx": 556562.5,
+		"ly": 222500,
+		"lz": 811.98,
+		"ux": 557890.06,
+		"uy": 223827.56,
+		"uz": 2139.54
+	},
+	"tightBoundingBox": {
+		"lx": 556562.5,
+		"ly": 222500,
+		"lz": 811.98,
+		"ux": 557267.29,
+		"uy": 223827.56,
+		"uz": 1057.39
+	},
+	"pointAttributes": [
+		"POSITION_CARTESIAN",
+		"COLOR_PACKED",
+		"INTENSITY",
+		"CLASSIFICATION"
+	],
+	"spacing": 6.09179210662842,
+	"scale": 0.01,
+	"hierarchyStepSize": 6
+}
