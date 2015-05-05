a66153d5b7f278ccb1e89a4c490cf62e06231e98
monodo <olivier.monod@ne.ch>
2015-05-05 14:13:16 +0200

    Clean map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,27 +1,31 @@
+/*** 
+* Initialize the OL3 2D map 
+* Method: initMapView
+* Parameters: none
+***/
 pv.map2D.initMapView = function () {
 
-    // base layers' attributions
-    pv.map2D.attributions = new ol.Attribution({
+    // Base layers' attributions
+    this.attributions = new ol.Attribution({
         html: pv.params.mapconfig.attributionsHtml
     });
 
     // From their CRS, get the projection system of the pointcloud and the 2D map
-    pv.map2D.mapProjection = ol.proj.get(pv.params.mapconfig.mapCRS);
-    pv.map2D.mapProjection.setExtent(pv.params.mapconfig.projectionExtent);
-    pv.map2D.pointCloudProjection = ol.proj.get(pv.params.mapconfig.pointCloudCRS);
+    this.mapProjection = ol.proj.get(pv.params.mapconfig.mapCRS);
+    this.mapProjection.setExtent(pv.params.mapconfig.projectionExtent);
+    this.pointCloudProjection = ol.proj.get(pv.params.mapconfig.pointCloudCRS);
 
-    // extent of the point cloud (with altitude)
+    // Extent of the point cloud (with altitude)
     var pointCloudExtentMin = pv.params.mapconfig.pointCloudExtentMin;
     var pointCloudExtentMax = pv.params.mapconfig.pointCloudExtentMax;
 
-    // point cloud extent in map CRS
-    var minWebCloud = ol.proj.transform([pointCloudExtentMin[0],pointCloudExtentMin[1]], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
-    var maxWebCloud = ol.proj.transform([pointCloudExtentMax[0],pointCloudExtentMax[1]], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
-
+    // Point cloud extent in map CRS
+    var minWebCloud = ol.proj.transform([pointCloudExtentMin[0],pointCloudExtentMin[1]], this.pointCloudProjection, this.mapProjection );
+    var maxWebCloud = ol.proj.transform([pointCloudExtentMax[0],pointCloudExtentMax[1]], this.pointCloudProjection, this.mapProjection );
     var pointCloudExtent = [minWebCloud[0], minWebCloud[1], maxWebCloud[0], maxWebCloud[1]];
     var pointCloudCenter = [(maxWebCloud[0] + minWebCloud[0]) / 2, (maxWebCloud[1] + minWebCloud[1]) / 2];
 
-    // draw the extent as box inside the map view
+    // Layer used to draw the point cloud extent
     var box = new ol.geom.LineString([
         minWebCloud, [maxWebCloud[0], minWebCloud[1]], maxWebCloud, [minWebCloud[0], maxWebCloud[1]], minWebCloud
     ]);
@@ -30,26 +34,6 @@ pv.map2D.initMapView = function () {
     var featureVector = new ol.source.Vector({
         features: [feature]
     });
-
-    var extentLayer = new ol.layer.Vector({
-        source: featureVector,
-        style: new ol.style.Style({
-            fill: new ol.style.Fill({
-                color: 'rgba(255, 255, 255, 0.2)'
-            }),
-            stroke: new ol.style.Stroke({
-                color: '#000000',
-                width: 2
-            }),
-            image: new ol.style.Circle({
-                radius: 3,
-                fill: new ol.style.Fill({
-                color: '#000000'
-                })
-            })
-        })
-    });
-
 
     var visibleBoundsLayer = new ol.layer.Vector({
         source: featureVector,
@@ -70,7 +54,8 @@ pv.map2D.initMapView = function () {
         })
     });
 
-    pv.map2D.toolLayer = new ol.layer.Vector({
+    // Layer used to display the tools drawings in 2D
+    this.toolLayer = new ol.layer.Vector({
         source: new ol.source.Vector({
         }),
         style: new ol.style.Style({
@@ -84,12 +69,12 @@ pv.map2D.initMapView = function () {
         })
     });
 
-    pv.map2D.camFrustumFeatureVector = new ol.source.Vector({
+    this.camFrustumFeatureVector = new ol.source.Vector({
         features: []
     });
 
     var camFrustumLayer = new ol.layer.Vector({
-        source: pv.map2D.camFrustumFeatureVector,
+        source: this.camFrustumFeatureVector,
         style: new ol.style.Style({
             fill: new ol.style.Fill({
                 color: 'rgba(255, 255, 255, 0.2)'
@@ -115,26 +100,26 @@ pv.map2D.initMapView = function () {
         undefinedHTML: '&nbsp;'
     });
 
-    pv.map2D.map = new ol.Map({
+    this.map = new ol.Map({
         controls: [
             new ol.control.ScaleLine(),
             mousePositionControl
         ],
         layers: [
-            pv.map2D.toolLayer,
-            extentLayer,
+            this.toolLayer,
             visibleBoundsLayer,
             camFrustumLayer
         ],
         target: 'map',
         view: new ol.View({
-            projection: pv.map2D.mapProjection,
+            projection: this.mapProjection,
             center: pointCloudCenter,
             extent: extent,
             zoom: pv.params.mapconfig.initialZoom
         })
     });
 
+    // Base layers can be WMS of WMTS, depending of config/config.js file
     if (pv.params.mapconfig.mapServiceType == 'WMTS') {
 
         var parser = new ol.format.WMTSCapabilities();
@@ -174,31 +159,32 @@ pv.map2D.initMapView = function () {
 
     } else {
         // WMS
-        pv.map2D.baseLayer = new ol.layer.Image({
+        this.baseLayer = new ol.layer.Image({
             extent: extent,
             source: new ol.source.ImageWMS({
-                attributions: [pv.map2D.attributions],
+                attributions: [this.attributions],
                 url: pv.params.mapconfig.mapServiceUrl,
                 params: {'LAYERS': pv.params.mapconfig.mapDefaultLayer},
                 serverType: /** @type {ol.source.wms.ServerType} */ ('mapserver')
             })
         });
 
-        var layersCollection = pv.map2D.map.getLayers();
-        layersCollection.insertAt(0, pv.map2D.baseLayer);
+        var layersCollection = this.map.getLayers();
+        layersCollection.insertAt(0, this.baseLayer);
     }
 
 };
-    
-/**
+
+/***
  * update the frustum in the map window according to the pv.scene3D.camera
+ * Method: updateMapFrustum
+ * Parameters: none
  */
 pv.map2D.updateMapFrustum = function (){
-    pv.map2D.camFrustum = new ol.geom.LineString([ [0,0], [0, 0] ]);
-
-    var feature = new ol.Feature(pv.map2D.camFrustum);
-    pv.map2D.camFrustumFeatureVector.clear();
-    pv.map2D.camFrustumFeatureVector.addFeature(feature);
+    this.camFrustum = new ol.geom.LineString([ [0,0], [0, 0] ]);
+    var feature = new ol.Feature(this.camFrustum);
+    this.camFrustumFeatureVector.clear();
+    this.camFrustumFeatureVector.addFeature(feature);
 
     var aspect = pv.scene3D.camera.aspect;
     var top = Math.tan( THREE.Math.degToRad( pv.scene3D.camera.fov * 0.5 ) ) * pv.scene3D.camera.near;
@@ -215,41 +201,48 @@ pv.map2D.updateMapFrustum = function (){
     camPos = pv.utils.toGeo(camPos);
     left = pv.utils.toGeo(left);
     right = pv.utils.toGeo(right);
-    camPos = ol.proj.transform([camPos.x, camPos.y], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
-    left = ol.proj.transform([left.x, left.y], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
-    right = ol.proj.transform([right.x, right.y], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
+    camPos = ol.proj.transform([camPos.x, camPos.y], this.pointCloudProjection, this.mapProjection );
+    left = ol.proj.transform([left.x, left.y], this.pointCloudProjection, this.mapProjection );
+    right = ol.proj.transform([right.x, right.y], this.pointCloudProjection, this.mapProjection );
 
-    pv.map2D.camFrustum.setCoordinates([camPos, left, right, camPos]);
+    this.camFrustum.setCoordinates([camPos, left, right, camPos]);
+
 };
 
-/**
- * Method: update the map extent in the map window
- * Parameters: none
- */
+/***
+* Update the map extent in the map window
+* Method: updateMapExtent
+* Parameters: none
+***/
 pv.map2D.updateMapExtent = function(){
+
     var geoExtent = pv.utils.toGeo(pv.scene3D.pointcloud.getVisibleExtent());
     var geoMin = ol.proj.transform([geoExtent.min.x, geoExtent.min.y], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
     var geoMax = ol.proj.transform([geoExtent.max.x, geoExtent.max.y], pv.map2D.pointCloudProjection, pv.map2D.mapProjection );
+
 };
 
-/**
-* Method: update the profile path on the 2D map
+/***
+* Update the profile path on the 2D map
+* Method: updateToolLayer
 * Parameters: Potree.profile (active profile);
 ***/
-
 pv.map2D.updateToolLayer = function (toolVertices) {
+
     if (!toolVertices){
         return;
     }
+
     var lineString = [];
     for (var i=0; i<toolVertices.length; i++) {
         var p = toolVertices[i];
         var pGeo = pv.utils.toGeo(p);
         lineString.push([pGeo.x, pGeo.y]);
     }
+
     var line = new ol.geom.LineString(lineString);
     var feature = new ol.Feature(line);
-    pv.map2D.toolLayer.getSource().clear();
-    pv.map2D.toolLayer.getSource().addFeature(feature);
-    pv.map2D.map.getView().fitExtent(pv.map2D.toolLayer.getSource().getExtent(), pv.map2D.map.getSize());
+    this.toolLayer.getSource().clear();
+    this.toolLayer.getSource().addFeature(feature);
+    this.map.getView().fitExtent(pv.map2D.toolLayer.getSource().getExtent(), pv.map2D.map.getSize());
 };
